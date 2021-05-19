e657210bd4833e98c104462419bc408a651a2bd0
AdrianoGalan <dricogalan@gmail.com>
2021-05-19 18:50:12 -0300

    con

--- src/main/java/br/edu/fateczl/WebServiceNotas/controller/AlunoController.java
+++ src/main/java/br/edu/fateczl/WebServiceNotas/controller/AlunoController.java
@@ -2,8 +2,14 @@ package br.edu.fateczl.WebServiceNotas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +19,28 @@ import br.edu.fateczl.WebServiceNotas.repository.AlunoRepository;
 @RestController
 @RequestMapping("/")
 public class AlunoController {
-	
+
 	@Autowired
 	private AlunoRepository aRep;
-	
+
+	public AlunoController() {
+		super();
+	}
+
 	@GetMapping("/aluno")
-	public List<Aluno> getAllAlunos(){
-		
+	public List<Aluno> getAllAlunos() {
+
 		List<Aluno> listaAluno = aRep.findAll();
 		return listaAluno;
-		
+
+	}
+	
+	@CrossOrigin
+	@PostMapping("/aluno")
+	public ResponseEntity<String> insertAluno(@Valid @RequestBody Aluno a) {
+		aRep.save(a);
+		return ResponseEntity.ok("Aluno adicionado");
+
 	}
 
 }
