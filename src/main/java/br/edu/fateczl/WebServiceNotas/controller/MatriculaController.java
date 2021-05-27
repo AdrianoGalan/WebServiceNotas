0217c31bd81fe2c92db2819796c58fdbf8e02966
AdrianoGalan <dricogalan@gmail.com>
2021-05-27 03:25:33 -0300

    matricula

--- src/main/java/br/edu/fateczl/WebServiceNotas/controller/MatriculaController.java
+++ src/main/java/br/edu/fateczl/WebServiceNotas/controller/MatriculaController.java
@@ -2,10 +2,15 @@ package br.edu.fateczl.WebServiceNotas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +48,14 @@ public class MatriculaController {
 
 		List<Disciplina> lisDisciplina = mRep.listDisciplinaByRaAluno(raAluno);
 		return lisDisciplina;
+
+	}
+	
+	@CrossOrigin
+	@PostMapping("/disciplina/add")
+	public ResponseEntity<String> insertMatricula(@Valid @RequestBody Matricula m) {
+		mRep.save(m);
+		return ResponseEntity.ok("Aluno matriculado");
 
 	}
 
