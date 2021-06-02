582d374ca880913d03fd55cae18a6e21604137dd
AdrianoGalan <dricogalan@gmail.com>
2021-06-02 18:44:33 -0300

    relatorio de faltas

--- src/main/java/br/edu/fateczl/WebServiceNotas/controller/AulaController.java
+++ src/main/java/br/edu/fateczl/WebServiceNotas/controller/AulaController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.edu.fateczl.WebServiceNotas.model.entity.Aula;
+import br.edu.fateczl.WebServiceNotas.model.entity.RelatorioDataAula;
 import br.edu.fateczl.WebServiceNotas.repository.AulaRepository;
+import br.edu.fateczl.WebServiceNotas.util.Convert;
 
 @RestController
 @RequestMapping("/")
@@ -40,6 +42,18 @@ public class AulaController {
 		
 	}
 	
+	@CrossOrigin
+	@GetMapping("/disciplina/aula/data/{codigo}")
+	public RelatorioDataAula getData(@PathVariable(value = "codigo") int codigo) {
+		
+		
+		Convert con = new Convert();		
+		List<Aula> listaAula = aRep.aulasDataByCodigoDisciplina(codigo);
+		RelatorioDataAula rDa = con.convertDatasEmData(listaAula);
+		
+		return rDa;
+		
+	}
 	
 
 }
